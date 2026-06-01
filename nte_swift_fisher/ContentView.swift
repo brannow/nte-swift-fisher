0fdd3387daa187d10f5edf2a2b105d614299fc5a
Benjamin Rannow <benjamin@rannow.de>
2026-06-01 14:16:44 +0700

    Initial Commit

---
+++ nte_swift_fisher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nte_swift_fisher
+//
+//  Created by Benjamin Rannow on 01.06.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nte_swift_fisher/nte_swift_fisherApp.swift
@@ -0,0 +1,17 @@
+//
+//  nte_swift_fisherApp.swift
+//  nte_swift_fisher
+//
+//  Created by Benjamin Rannow on 01.06.26.
+//
+
+import SwiftUI
+
+@main
+struct nte_swift_fisherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
